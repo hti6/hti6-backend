56386a310f5e8a134202355845d06c524c60c853
neoko <33611354+neokofg@users.noreply.github.com>
2024-11-07 22:51:55 +0300

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -22,7 +22,7 @@ final readonly class LoginController extends Controller
 
         if (Auth::guard('admin')
             ->attempt(['login' => $dto['login'], 'password' => $dto['password']])) {
-            $user = Auth::user();
+            $user = Auth::guard('admin')->user();
 
             $token = $user->createToken('auth-token', ['role:user','role:admin'])->plainTextToken;
         } else if (Auth::guard('web')
