2d953a45d1166ce28b42a3a86b1314123e189e76
neoko <33611354+neokofg@users.noreply.github.com>
2024-11-07 22:50:56 +0300

    Update Admin.php

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Model
 {
     use HasFactory;
     use HasUlids;
+    use HasApiTokens;
 
     protected $table = 'admins';
 
