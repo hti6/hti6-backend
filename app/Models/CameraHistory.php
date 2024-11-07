f624390414e284f116f2350f1664a582e8206d32
neoko <33611354+neokofg@users.noreply.github.com>
2024-11-07 18:16:08 +0300

    Update CameraHistory.php

--- app/Models/CameraHistory.php
+++ app/Models/CameraHistory.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CameraHistory extends Model
 {
-    //
+    use HasUlids;
+
+    protected $table = 'camera_history';
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function camera(): BelongsTo
+    {
+        return $this->belongsTo(Camera::class, 'camera_id', 'id');
+    }
 }
